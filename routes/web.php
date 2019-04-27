ab788176225dab8952fb39fd7fc8b0039f4896ab
Carla Ramirez <carla.ramirez.rojas@gmail.com>
2019-04-27 17:27:21 -0400

    introduction middleware

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/contacts/create', 'ContactController@create')->name('create');
-Route::post('/contacts/create', 'ContactController@store')->name('store');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/contacts/create', 'ContactController@create')->name('create');
+    Route::post('/contacts/create', 'ContactController@store')->name('store');
+});
+
+
 
 //Route::resource('contacts','ContactController');
